1034d1b9e7f7ac115ad5a4500c31648aa787d485
Ioannis Makris <imak@imak.gr>
2026-07-10 14:42:19 +0200

    style: align test macro continuations

--- tests/test_macros.h
+++ tests/test_macros.h
@@ -2,23 +2,23 @@
 
 #include <iostream>
 
-#define TEST_ASSERT(cond, msg) \
-    do { \
-        if (!(cond)) { \
+#define TEST_ASSERT(cond, msg)                                                         \
+    do {                                                                               \
+        if (!(cond)) {                                                                 \
             std::cerr << "FAIL: " << msg << " (line " << __LINE__ << ")" << std::endl; \
-            return false; \
-        } \
+            return false;                                                              \
+        }                                                                              \
     } while (0)
 
-#define RUN_TEST(test_fn) \
-    do { \
-        std::cout << "Running " << #test_fn << "... "; \
-        if (test_fn()) { \
-            std::cout << "OK" << std::endl; \
-            ++passed; \
-        } \
-        else { \
-            std::cout << "FAIL" << std::endl; \
-            ++failed; \
-        } \
+#define RUN_TEST(test_fn)                                                              \
+    do {                                                                               \
+        std::cout << "Running " << #test_fn << "... ";                                 \
+        if (test_fn()) {                                                               \
+            std::cout << "OK" << std::endl;                                            \
+            ++passed;                                                                  \
+        }                                                                              \
+        else {                                                                         \
+            std::cout << "FAIL" << std::endl;                                          \
+            ++failed;                                                                  \
+        }                                                                              \
     } while (0)
